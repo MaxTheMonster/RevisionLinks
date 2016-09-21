00903924f3c7830fa2af4d597cae857b4d086fc9
Max Ungless <maxungless@gmail.com>
2016-09-21 20:38:36 +0100

    Fixed Django URL Bugs and added styling

--- RevisionLinks/urls.py
+++ RevisionLinks/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^gcse/$', views.gcse, name="gcse"),
     url(r'^alevel/$', views.alevel, name="alevel"),
-    url(r'^alevel/(?P<subject_name>[\w+.])/', views.ALevelSubject, name="alevelsubject"),
-    url(r'^gcse/(?P<subject_name>[\w+.])/', views.GCSESubject, name="gcsesubject"),
+    url(r'^alevel/(?P<subject_name>\w+)/', views.ALevelSubject, name="alevelsubject"),
+    url(r'^gcse/(?P<subject_name>\w+)/', views.GCSESubject, name="gcsesubject"),
 ]
